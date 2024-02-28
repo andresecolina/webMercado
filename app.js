7bd22f94a0c006a7efc5050127bb2a36b46506cb
andresecolina <andresedavidcolina@gmail.com>
2024-02-27 22:02:00 -0500

    arregle cambios app.js

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express')
-const app = express();
-const port = process.env.PORT || 3000; 
+const app = express(); 
 const path = require('path')
- 
+const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve('./views/home.html'));
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
 });
 
 
 
 app.get ("/register", (req, res)=>{
-    res.sendFile(path.resolve("./views/register.html"))
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
 });
 
 app.get ("/login", (req, res)=>{
-    res.sendFile(path.resolve("./views/login.html"))
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
 });
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
