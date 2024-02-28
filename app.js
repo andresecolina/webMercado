32817a6cca7481781fa1fa903d646a7c96711377
andresecolina <andresedavidcolina@gmail.com>
2024-02-27 22:10:50 -0500

    arreglar error

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+    res.sendFile(path.resolve(__dirname,'./src/views/home.html'));
 });
 
 
 
 app.get ("/register", (req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+    res.sendFile(path.resolve(__dirname,"./src/views/register.html"))
 });
 
 app.get ("/login", (req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+    res.sendFile(path.resolve(__dirname,"./src/views/login.html"))
 });
 
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
