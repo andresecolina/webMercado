02941949296950dd54ebddadb2676ed8cac1a7e9
andresecolina <andresedavidcolina@gmail.com>
2024-02-27 21:26:43 -0500

    arregle error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'/views/home.html'));
 });
 
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+
 
 app.get ("/register", (req, res)=>{
     res.sendFile(path.join(__dirname, "/views/register.html"))
@@ -19,5 +19,5 @@ app.get ("/login", (req, res)=>{
     res.sendFile(path.join(__dirname, "/views/login.html"))
 });
 
-
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
